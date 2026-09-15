b577a48a1aac7759d22b8fcbca78a915a6688f71
Nur Azizah <nur.azizah56@ui.ac.id>
2026-09-16 04:50:27 +0700

    feat: daftarkan url create_experience

--- main/urls.py
+++ main/urls.py
@@ -3,10 +3,11 @@ from django.urls import path
 from main.views import (
     show_main, 
     show_experience, 
+    create_experience,
     show_education, 
     show_skills, 
     show_achievements,
-    show_certifications
+    show_certifications,
 )
 
 app_name = "main"
@@ -14,6 +15,7 @@ app_name = "main"
 urlpatterns = [
     path("", show_main, name="show_main"),
     path("experience/", show_experience, name="show_experience"),
+    path("experience/add/", create_experience, name="create_experience"),
     path("education/", show_education, name="show_education"),
     path("skills/", show_skills, name="show_skills"),
     path("achievements/", show_achievements, name="show_achievements"),

7fc8f0910aba09f564c606702a95bd23b27dfa0b
Nur Azizah <nur.azizah56@ui.ac.id>
2026-09-16 15:06:11 +0700

    feat: tambah fitur delete experience

--- main/views.py
+++ main/views.py
@@ -92,3 +92,13 @@ def get_experience_json(request):
 
     experiences_json = serializers.serialize("json", experiences)
     return HttpResponse(experiences_json, content_type="application/json")
+
+def delete_experience(request, experience_id):
+    experience = get_object_or_404(Experience, pk=experience_id)
+
+    if request.method == "POST":
+        experience.delete()
+        messages.success(request, "Experience berhasil dihapus!")
+        return redirect("main:show_experience")
+
+    return redirect("main:show_experience")
